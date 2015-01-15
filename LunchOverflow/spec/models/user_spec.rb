b8363fc63bff39aa9af1f67b74389f34b7d3f376
Brian Borge <brianborge@gmail.com>
2015-01-15 18:13:57 -0500

    Add validation tests for User

--- LunchOverflow/spec/models/user_spec.rb
+++ LunchOverflow/spec/models/user_spec.rb
@@ -7,4 +7,11 @@ describe User do
     it { should have_db_column(:email).of_type(:string) }
     it { should have_db_column(:password_digest).of_type(:string) }
   end
+
+  describe "Validation tests" do
+    let(:blanks) { [nil, ''] }
+    it { should have_secure_password }
+    it { should have_valid(:user_name).when("Brian") }
+    it { should_not have_valid(:user_name).when(*blanks) }
+  end
 end
