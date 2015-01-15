02375d2f664379f825f38e24c972afcc98120731
Kevin <kvnkwon@gmail.com>
2015-01-15 17:36:39 -0500

    Fix syntax error in migrations -NULL:-

--- LunchOverflow/db/migrate/20150115213756_create_users.rb
+++ LunchOverflow/db/migrate/20150115213756_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_name, null => false
-      t.string :email, null => false
-      t.string :pasword_digest, null => false
+      t.string :user_name, null: false
+      t.string :email, null: false
+      t.string :pasword_digest, null: false
       t.timestamps
     end
   end
--- LunchOverflow/db/migrate/20150115213813_create_posts.rb
+++ LunchOverflow/db/migrate/20150115213813_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, null => false
-      t.string :content, null => false
+      t.string :title, null: false
+      t.string :content, null: false
       t.string :address
       t.references :user
       t.timestamps
