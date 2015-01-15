6714fb08424226588e31f2bea74b2fc5e2dcd7f9
Brian Borge <brianborge@gmail.com>
2015-01-15 18:36:05 -0500

    Add association tests, email validation

--- LunchOverflow/spec/models/user_spec.rb
+++ LunchOverflow/spec/models/user_spec.rb
@@ -13,5 +13,12 @@ describe User do
     it { should have_secure_password }
     it { should have_valid(:user_name).when("Brian") }
     it { should_not have_valid(:user_name).when(*blanks) }
+    it { should have_valid(:email).when("brian@gmail.com") }
+    it { should_not have_valid(:email).when("brian@brian") }
+  end
+
+  describe "Association tests" do
+    it { have_many(:posts) }
+    it { have_many(:comments) }
   end
 end
