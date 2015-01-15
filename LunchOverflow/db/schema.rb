ff33c82d16e3fc596c401bc2e8aa3a36920f8736
Kevin <kvnkwon@gmail.com>
2015-01-15 17:57:51 -0500

    Create migration to fix spelling error in users table

--- LunchOverflow/db/schema.rb
+++ LunchOverflow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115213824) do
+ActiveRecord::Schema.define(version: 20150115225322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 20150115213824) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "user_name",      null: false
-    t.string   "email",          null: false
-    t.string   "pasword_digest", null: false
+    t.string   "user_name",       null: false
+    t.string   "email",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest", null: false
   end
 
 end
