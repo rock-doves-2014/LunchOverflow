cfa244305e35b4e7a42bffc2fccd645db86077fe
Kevin <kvnkwon@gmail.com>
2015-01-16 13:44:44 -0500

    Add create method in post controller with corresponding view

--- LunchOverflow/app/controllers/posts_controller.rb
+++ LunchOverflow/app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Posts.all
+  end
+
   def new
+    @post = Post.new
+  end
+
+  def create
+    user = User.find(session[:user_id])
+    @post = user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address, :user_id)
   end
 
 end
