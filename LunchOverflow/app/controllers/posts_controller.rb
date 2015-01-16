df9cbf6899f7ca31e0f78b1a125227dbb5db2f3f
Kevin <kvnkwon@gmail.com>
2015-01-16 14:42:00 -0500

    Create edit and update methods in posts controller with corresponding view

--- LunchOverflow/app/controllers/posts_controller.rb
+++ LunchOverflow/app/controllers/posts_controller.rb
@@ -18,6 +18,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post.id)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def post_params
