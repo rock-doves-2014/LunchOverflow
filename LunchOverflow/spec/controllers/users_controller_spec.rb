f31e0e8be0fd9aad5a2fa9dfd0eee26dc0745fd1
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-15 19:22:43 -0500

    Add test to User controller#new

--- LunchOverflow/spec/controllers/users_controller_spec.rb
+++ LunchOverflow/spec/controllers/users_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
-  describe UsersController do
-    let(:user) { FactoryGirl.create :user }
+describe UsersController do
+   let(:user) { FactoryGirl.create :user }
 
-    describe "#new" do
+   describe "#new" do
 
-      it "assigns the todos to User.new" do
-        get :new
-        expect(assign(:user).to be_a_new(User))
-      end
-    end
-  end
+     it "assigns the todos to User.new" do
+       get :new
+       expect(assign(:user).to be_a_new(User))
+     end
+   end
+end
