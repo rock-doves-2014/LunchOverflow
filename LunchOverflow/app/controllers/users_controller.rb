98f59e2514d27b0c9dd329961ff09abd7c7776eb
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-15 20:00:36 -0500

    Add method user_params to get the strong params

--- LunchOverflow/app/controllers/users_controller.rb
+++ LunchOverflow/app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+    private
+  
+    def user_params
+      params.require(:user).permit(:user_name, :email, :password)
+    end
 end
