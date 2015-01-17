68b3eb6a6e52092f4e28793ed80c441ebb73259e
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-17 12:59:15 -0500

    Add upvote method on post class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,11 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to posts_path
+  end
   private
 
   def post_params
