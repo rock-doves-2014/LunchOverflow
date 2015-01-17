35418a589125182b09056921e35d6542814c2e83
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-17 12:59:59 -0500

    Add downvote method to post class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,13 @@ class PostsController < ApplicationController
     @post.upvote_by current_user
     redirect_to posts_path
   end
+
+  def downvote
+   @post = Post.find(params[:id])
+   @post.downvote_by current_user
+   redirect_to posts_path
+  end
+
   private
 
   def post_params
