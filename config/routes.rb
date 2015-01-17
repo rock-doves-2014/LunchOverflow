a91607fc92f83a92303be2be630279c82433e70a
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-17 13:02:38 -0500

    Add upvote link and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   resources :posts do
     resources :comments
+
+    member do
+      put 'upvote', to: 'posts#upvote'
+    end
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
